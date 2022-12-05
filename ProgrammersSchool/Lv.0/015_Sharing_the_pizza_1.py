2fe72067f46b301307997961d8eef569c5fa6409
Superb777Sugi <119590110+Superb777Sugi@users.noreply.github.com>
2022-12-05 20:36:52 +0900

    Create 015_Sharing_the_pizza_1.py

---
+++ ProgrammersSchool/Lv.0/015_Sharing_the_pizza_1.py
@@ -0,0 +1,15 @@
+def solution(n):
+    """
+    >>> solution(7)
+    1
+    >>> solution(1)
+    1
+    >>> solution(15)
+    3
+    """
+    pizza = n // 7
+    if (n % 7) == 0:
+        pizza = pizza
+    else:
+        pizza = pizza + 1
+    return pizza
