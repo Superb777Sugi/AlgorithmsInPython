3ee8c39ad3ea4583111ff517aa05781dc96c1898
Superb777Sugi <119590110+Superb777Sugi@users.noreply.github.com>
2022-12-06 20:55:47 +0900

    Update .pre-commit-config.yaml

--- ProgrammersSchool/Lv.0/017_Sharing_the_pizza_3.py
+++ ProgrammersSchool/Lv.0/017_Sharing_the_pizza_3.py
@@ -1,11 +1,11 @@
 def solution(slice: int, n: int) -> int:
-	"""
-	>>> solution(7, 10)
-	2
-	>>> solution(4, 12)
-	3
-	"""
-	pizza = 1 
-	while (pizza * slice) < n: 
-		pizza += 1 
-	return pizza
+    """
+    >>> solution(7, 10)
+    2
+    >>> solution(4, 12)
+    3
+    """
+    pizza = 1
+    while (pizza * slice) < n:
+        pizza += 1
+    return pizza
