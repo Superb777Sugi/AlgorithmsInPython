7055e04cb1809a8759bf3db74937907b40e8b8d8
Superb777Sugi <119590110+Superb777Sugi@users.noreply.github.com>
2022-12-08 17:30:24 +0900

    Create 023_reverse_string.py

---
+++ ProgrammersSchool/Lv.0/023_reverse_string.py
@@ -0,0 +1,9 @@
+def solution(my_string: str) -> str:
+    """
+    >>> solution("jaron")
+    "noraj"
+    >>> solution("bread")
+    "daerb"
+    """
+    answer = my_string[::-1]
+    return answer
